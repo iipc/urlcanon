bf069e11719a15bfb0ac4a98c93b29612a558363
Noah Levitt <nlevitt@archive.org>
2018-05-02 14:17:12 -0700

    bump dev version number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 setuptools.setup(
         name='urlcanon',
         description='url canonicalization library for python and java',
-        version='0.1.dev20',
+        version='0.1.dev21',
         packages=['urlcanon'],
         url='https://github.com/iipc/urlcanon',
         install_requires=dependencies,
