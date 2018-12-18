3447f043add34923f20c50238ef103cc68a0afc5
Noah Levitt <nlevitt@archive.org>
2018-12-17 16:03:47 -0800

    python 0.2.1 release
    idna 2.8

--- setup.py
+++ setup.py
@@ -19,16 +19,16 @@ limitations under the License.
 
 import setuptools
 
-dependencies = ['idna>=2.5']
+dependencies = ['idna>=2.5,<=2.8']
 try:
     import ipaddress
 except ImportError:
-    dependencies.append('py2-ipaddress>=3.4.1')
+    dependencies.append('py2-ipaddress>=3.4.1,<=3.4.1')
 
 setuptools.setup(
         name='urlcanon',
         description='url canonicalization library for python and java',
-        version='0.2.dev27',
+        version='0.2.1',
         packages=['urlcanon'],
         url='https://github.com/iipc/urlcanon',
         install_requires=dependencies,
@@ -40,6 +40,7 @@ setuptools.setup(
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Programming Language :: Java',
             'Topic :: Internet :: WWW/HTTP',],)
 
