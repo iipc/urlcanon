f6c8d41a12991d01c4c4246348a5eae61dd86c4e
Noah Levitt <nlevitt@archive.org>
2019-02-22 08:45:24 +0000

    test exposing surt() bug when host not domain name

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 setuptools.setup(
         name='urlcanon',
         description='url canonicalization library for python and java',
-        version='0.3.dev29',
+        version='0.3.dev30',
         packages=['urlcanon'],
         url='https://github.com/iipc/urlcanon',
         install_requires=dependencies,
